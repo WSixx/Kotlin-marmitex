6209c73d0ac21ae2bcdd0bcb3f0d7d8712ee4444
WSixx <lucas.2007s@gmail.com>
2021-05-05 18:03:34 -0300

    Adicionando DOTENV - file not found(ENV)

--- app/src/main/java/br/com/lucad/kotlinmarmitex/LoginActivity.kt
+++ app/src/main/java/br/com/lucad/kotlinmarmitex/LoginActivity.kt
@@ -1,11 +1,24 @@
 package br.com.lucad.kotlinmarmitex
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import io.github.cdimascio.dotenv.dotenv
+
+
+val dotenv = dotenv {
+    directory = "E:\\Kotlin\\Prj\\KotlinMarmitex\\app\\src\\main\\assets\\env"
+    filename = "env" // use filename env, instead of .env
+} // <---- 1. Configure dotenv
 
 class LoginActivity : AppCompatActivity() {
+
+    private val ev = dotenv["MY_EV"] // <---- 2. Get value from dotenv
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+        Toast.makeText(this, "Valor da $ev", Toast.LENGTH_LONG).show()
     }
 }
