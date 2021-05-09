efd09f43e5b767242d23b72f7989b151230ee92f
WSixx <lucas.2007s@gmail.com>
2021-05-09 13:17:07 -0300

    Tela de editar nome/email

---
+++ app/src/main/java/br/com/lucad/kotlinmarmitex/EditProfile.kt
@@ -0,0 +1,11 @@
+package br.com.lucad.kotlinmarmitex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+    }
+}
