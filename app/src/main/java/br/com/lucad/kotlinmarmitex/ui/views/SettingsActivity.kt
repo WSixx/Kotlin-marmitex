0d85a7d3ced000ab874314616f55f516e384169e
WSixx <lucas.2007s@gmail.com>
2021-05-11 18:30:16 -0300

    Coloquei uma toolbar em Settings com OnBackPressed

--- app/src/main/java/br/com/lucad/kotlinmarmitex/ui/views/SettingsActivity.kt
+++ app/src/main/java/br/com/lucad/kotlinmarmitex/ui/views/SettingsActivity.kt
@@ -2,12 +2,25 @@ package br.com.lucad.kotlinmarmitex.ui.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import br.com.lucad.kotlinmarmitex.R
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar_settings);
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toolbar.title = "Configuracoes"
+        actionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_ios_24)
+
+        toolbar.setNavigationOnClickListener{
+            onBackPressed()
+        }
+
 
     }
 }
